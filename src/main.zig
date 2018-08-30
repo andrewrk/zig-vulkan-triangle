6c00441d117b307fc03b5955fbe14675a6336d33
Andrew Kelley <superjoe30@gmail.com>
2018-08-30 17:40:30 -0400

    cleanup stub

--- src/main.zig
+++ src/main.zig
@@ -93,6 +93,45 @@ pub fn main() !void {
         try drawFrame();
     }
     try checkSuccess(c.vkDeviceWaitIdle(global_device));
+
+    cleanup();
+}
+
+fn cleanup() void {
+    // TODO
+    //for (size_t i = 0; i < MAX_FRAMES_IN_FLIGHT; i++) {
+    //    vkDestroySemaphore(device, renderFinishedSemaphores[i], nullptr);
+    //    vkDestroySemaphore(device, imageAvailableSemaphores[i], nullptr);
+    //    vkDestroyFence(device, inFlightFences[i], nullptr);
+    //}
+
+    //vkDestroyCommandPool(device, commandPool, nullptr);
+
+    //for (auto framebuffer : swapChainFramebuffers) {
+    //    vkDestroyFramebuffer(device, framebuffer, nullptr);
+    //}
+
+    //vkDestroyPipeline(device, graphicsPipeline, nullptr);
+    //vkDestroyPipelineLayout(device, pipelineLayout, nullptr);
+    //vkDestroyRenderPass(device, renderPass, nullptr);
+
+    //for (auto imageView : swapChainImageViews) {
+    //    vkDestroyImageView(device, imageView, nullptr);
+    //}
+
+    //vkDestroySwapchainKHR(device, swapChain, nullptr);
+    //vkDestroyDevice(device, nullptr);
+
+    //if (enableValidationLayers) {
+    //    DestroyDebugReportCallbackEXT(instance, callback, nullptr);
+    //}
+
+    //vkDestroySurfaceKHR(instance, surface, nullptr);
+    //vkDestroyInstance(instance, nullptr);
+
+    //glfwDestroyWindow(window);
+
+    //glfwTerminate();
 }
 
 fn initVulkan(allocator: *Allocator, window: *c.GLFWwindow) !void {
